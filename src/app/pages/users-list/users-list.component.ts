74fad644d87b8081a292262aa1a50232903133d0
Doummar M.alzahabi <98951369+Doummarzhb@users.noreply.github.com>
2025-08-30 02:42:47 +0300

    Edit image

--- src/app/pages/users-list/users-list.component.ts
+++ src/app/pages/users-list/users-list.component.ts
@@ -5,12 +5,21 @@ import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { Router } from '@angular/router';
 import { UsersService , User } from '../../core/state/users.service';
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.scss']
+  styleUrls: ['./users-list.component.scss'],
+  animations: [
+    trigger('titleAnimation', [
+      state('void', style({ opacity: 0, transform: 'scale(0.8)' })),
+      transition(':enter', [
+        animate('600ms ease-out', style({ opacity: 1, transform: 'scale(1)' }))
+      ])
+    ])
+  ]
 })
+
 export class UsersListComponent implements OnInit {
   users: User[] = [];
   displayedUsers: User[] = [];
@@ -55,6 +64,6 @@ export class UsersListComponent implements OnInit {
   }
 
   goToDetail(id: number) {
-    this.router.navigate(['/user', id]);
+    this.router.navigate(['/users', id]);
   }
 }
